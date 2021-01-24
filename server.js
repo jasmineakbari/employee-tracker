8dc3147433c03d4d1b7ba5070f9dafabdf3868ea
unknown <jasmineakbari@yahoo.com>
2021-01-24 11:26:11 -0500

    removed db added initial inquirer prompt

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const inquirer = require('inquirer');
-const db = require('./db/database');
-
 
 const promptUser = () => {
     inquirer
         // provides employee track option based on employee, deparment, roles
         .prompt({
-
+            type: 'list',
+            name: 'options',
+            message: 'Select an option to to continue.',
+            choices: ['View Departments', 'View all Roles', 
+            'View current Employees', 'Add a Department', 
+            'Add a role', 'Add an Employee', 'Update an Employees Info']
         })
+        // How to handle each case
+
 }
 
 module.exports = { promptUser };
+
+promptUser();
