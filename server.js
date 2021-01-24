6a8fc73107e802a83b6e54bd8d14ffb27f4d738e
unknown <jasmineakbari@yahoo.com>
2021-01-24 16:02:42 -0500

    corrected circular dependancy error

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
-//const { promptUser } = require('../server');
 
 const promptUser = () => {
     inquirer
@@ -22,37 +20,23 @@ const promptUser = () => {
                 case 'View all Roles':
                     break;
                 case 'View current Employees':
-                    // connection to the database
-                    const connection = mysql.createConnection({
-                        host: 'localhost',
-                        // Your MySQL username
-                        user: 'root',
-                        password: 'password',
-                        database: 'employees'
-                    });
-
-                    connection.connect(err => {
-                        if (err) throw err;
-                        console.log('connected as id ' + connection.threadId + '\n');
-                        displayEmps();
-                    });
-
-                    // function to view employees
-                    displayEmps = () => {
-                        connection.query('SELECT * FROM employee', function(err, res) {
-                            if (err) throw err;
-                            console.table(res);
-                            promptUser();
-                        })
-                    }
+                    displayEmps();
+                    break;
+                case 'Add a Department':
+                    break;
+                case 'Add a role':
+                    break;
+                case 'Add an Employee':
+                    addEmp();
+                    break;
+                case 'Update existing Employees Info':
+                    editEmp();
                     break;
             }
         })
 };
 
-
-
-//module.exports = { promptUser };
-//const { displayEmps } = require('./lib/employees');
+module.exports = { promptUser }
+const { displayEmps, addEmp, editEmp } = require('./lib/employees');
 
 promptUser();
